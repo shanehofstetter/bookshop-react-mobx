c25d756ab2b3ffd3701fb0e0525b09c150628934
Shane Hofstetter <shane.hofstetter@gmail.com>
2018-12-26 12:37:54 +0100

    reorganize app, add manifest

--- app/javascript/src/index.jsx
+++ app/javascript/src/index.jsx
@@ -3,8 +3,9 @@ import * as ReactDOM from 'react-dom';
 
 import Root from "./root";
 
-import '../../../../semantic-ui-theme/dist/semantic.min.css';
+import '../../../semantic-ui-theme/dist/semantic.min.css';
 import 'react-datetime/css/react-datetime.css'
+import './assets/stylesheets/app.scss'
 
 document.addEventListener('DOMContentLoaded', () => {
     ReactDOM.render(<Root/>, document.getElementById('bookshop-app'));
